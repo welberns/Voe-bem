c390bd1503920861b082190609279fd3c3134920
welberNS <welberns@hotmail.com>
2024-10-11 23:47:45 -0300

    trocatema

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
-Let darkmode =localstorage.getItem('darkmode')
-const trocaTema = document.getElement.ById('trocaTema) 
+let darkmode =localstorage.getItem('darkmode')
+const trocaTema = document.getElement.ById('trocaTema') 
 
 const enableDarkmode = () => {
 document.body.classList.add('darkmode') 
-localstorage.setItem('darkmode', 'active' (
+localstorage.setItem('darkmode', 'active')
 } 
 
 const disableDarkmode = () => {
 document.body.classList.remove('darkmode') 
-localstorage.setItem.('darkmode', null) 
+localstorage.setItem('darkmode', null) 
 } 
 
 trocaTema.addEventListener('click',() => {
-darkmode! == "active" ? enableDarkmode() : disableDarkmode() 
+darkmode !== "active" ? enableDarkmode() : disableDarkmode() 
 }) 
